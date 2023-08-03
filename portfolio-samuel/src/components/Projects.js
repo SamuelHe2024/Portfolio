18e22de8701adc8b06749cc5da87a85782a93812
Samuel <david1997h@gmail.com>
2023-08-03 11:13:56 -0500

    refactor: changed landing page styling

--- portfolio-samuel/src/components/Projects.js
+++ portfolio-samuel/src/components/Projects.js
@@ -22,33 +22,33 @@ export default function Projects(){
 
     return(
         <div>
-            <div className="h-[40vh] bg-gradient-to-b from-gunmetal to-black">
+            <div className="h-[20vh] bg-gradient-to-b from-gunmetal to-slate-900">
             </div>
                         
-            <div className="h-full bg-black">
+            <div className="h-full border-b border-outer-space">
                 <div className="h-2/5">
-                    </div>
-                    <div className="grid h-3/5 bg-black">
-                        <div className="grid md:grid-cols-2 lg:grid-cols-3">
-                            <div className="text-keppel m-auto text-4xl font-mono lg:col-span-3 md:col-span-2">
-                                Repositories:
-                            </div>
-                            {projectList.map(
-                                project=>
-                                    <Card 
-                                        project_name = {project.name}
-                                        url = {project.html_url}
-                                        language = {project.language ? project.language : "N/A"}
-                                        color = {project.language ? colors[project.language].color : ''}
-                                        description = {project.description}
-                                    >
-                                    </Card>
-                            )}
-                    </div>
-                        
+                </div>
+                <div className="grid h-3/5 bg-slate-900">
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-auto mb-[10vh]">
+                        <div className="font-bold mt-[10vh] text-keppel/80 py-1 mb-4 m-auto border-b border-b-keppel/80 text-2xl md:text-3xl lg:text-4xl font-mono lg:col-span-3 md:col-span-2">
+                            PUBLIC REPOSITORIES
+                        </div>
+                        {projectList.map(
+                            project=>
+                                <Card 
+                                    project_name = {project.name}
+                                    url = {project.html_url}
+                                    language = {project.language ? project.language : "NOT STARTED"}
+                                    color = {project.language ? colors[project.language].color : ''}
+                                    description = {project.description}
+                                >
+                                </Card>
+                        )}
+                    </div>  
                 </div>
             </div>
-                
+            <div className="h-[30vh] bg-slate-900">
+            </div>
 
         </div>
        
---
+++ portfolio-samuel/src/components/WorkExperience.js
@@ -0,0 +1,7 @@
+export default function WorkExperience(props){
+    return(
+        <div>
+            
+        </div>
+    )
+}
