26ab36348f852c260aaf9465d3922c5d644d6b8d
Samuel <david1997h@gmail.com>
2023-08-04 15:53:53 -0500

    REFACTOR: added a new color

--- portfolio-samuel/src/components/Landing Card.js
+++ portfolio-samuel/src/components/Landing Card.js
@@ -9,7 +9,7 @@ export default function LandingCard(){
         <div className = "fixed grid grid-flow-row bg-gunmetal w-screen h-screen grid-rows-6 grid-cols-3">
             <div className="row-span-1"></div>
             
-            <div className="col-span-3 row-span-3 m-auto border-b bg-black/30 rounded-xl p-16 shadow-teal-400 shadow-sm border-keppel">
+            <div className="col-span-3 row-span-3 m-auto border-b bg-slate-900 rounded-xl p-16 shadow-teal-400 shadow-sm border-keppel">
                 <div className="text-keppel mb-8 xl:mb-12 text-4xl xl:text-5xl row-span-1 font-mono overflow-hidden whitespace-nowrap animate-typing1">Samuel He</div>
                 <img src={profile} alt={empty}  className="m-auto mb-8 col-span-3 my-auto w-[125px] xl:w-[200px] border-keppel border-2 rounded-full"/>
                 <div className="grid grid-cols-4 mb-8">
