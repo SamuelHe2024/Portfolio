ca1c0613ba28d60bfe3081f1068011476036e544
weewoo123 <samuelhe2014@gmail.com>
2025-04-19 15:23:54 -0500

    feature/site-reset

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import LandingCard from './components/Landing Card';
-import Projects from './components/Projects';
-import WorkExperience from './components/WorkExperience';
-
 
 
 function App() {
   return (
     <div>
-    <LandingCard></LandingCard>
-    <div className='h-[90vh]'></div>
-    <WorkExperience></WorkExperience>
-    <Projects></Projects>
     </div>
   );
 }
